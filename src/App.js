fa6f445790118039ffe225fa910de15434b96f14
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2022-01-31 18:46:34 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3> Okey i'm going to start from scratch... </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
