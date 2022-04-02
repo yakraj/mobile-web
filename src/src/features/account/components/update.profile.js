4a996aeb117e7b3489d199c4f41c73b375d8cbeb
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2022-04-02 21:38:30 +0530

    edit profile

--- src/src/features/account/components/update.profile.js
+++ src/src/features/account/components/update.profile.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Topbar } from "./../../../components/global/topbar";
 import Profile from "../../../../assets/profile.jpg";
 import "./updateprofile.css";
 import { Link } from "react-router-dom";
+import { UserContext } from "./../../../services/user.contex";
 export const EditProfile = () => {
+  const { usercrd, Updateavatar, UpdateName, UpdateMobile } =
+    useContext(UserContext);
+
+  const [icon, seticon] = useState();
+  useEffect(() => {
+    seticon("");
+  }, [seticon]);
+
+  const [firstname, setfirstname] = useState(usercrd.firstname);
+  const [lastname, setlastname] = useState(usercrd.lastname);
+  const [mobile, setmobile] = useState(usercrd.mobile);
   return (
     <>
       <Topbar bcC="#AAF3F5" title="Edit Profile" />
@@ -26,21 +38,51 @@ export const EditProfile = () => {
       {/* user name change container */}
       <div className="user-name-update">
         <h4>First Name</h4>
-        <input type="text" placeholder="Yakraj" />
+        <input
+          value={firstname}
+          onChange={(e) => setfirstname(e.target.value)}
+          type="text"
+        />
         <h4>Last Name</h4>
-        <input type="text" placeholder="Pariyar" />
+        <input
+          defaultValue={lastname}
+          onChange={(e) => setlastname(e.target.value)}
+          type="text"
+        />
       </div>
       {/* update names button */}
       <div class="update-button">
-        <div> Update Details</div>
+        <div
+          onClick={() => {
+            UpdateName(firstname, lastname, usercrd.username);
+
+            window.alert("Your Name will be updated as ");
+            setfirstname("");
+            setlastname("");
+          }}
+        >
+          Update Details
+        </div>
       </div>
       {/* update Mobile number */}
       <div class="update-user-mobile">
         <div class="contact-details-edit">Contact Details</div>
         <div class="contact-details-mobile-container">
-          <input type="text" placeholder="7709543082" />
+          <input
+            value={mobile}
+            onChange={(e) => setmobile(e.target.value)}
+            type="text"
+          />
           <div class="update-button">
-            <div> Update </div>
+            <div
+              onClick={() => {
+                UpdateMobile(mobile, usercrd.username);
+                window.alert("Your mobile number will be updated very sortly.");
+              }}
+              touchab
+            >
+              Update
+            </div>
           </div>
         </div>
       </div>
